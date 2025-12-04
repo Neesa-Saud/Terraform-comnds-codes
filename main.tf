08c574e5306a0a97a289078398d09d89603d49ce
Neesa-Saud <nishasaud2061@gmail.com>
2025-12-04 21:47:54 +0545

    added

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws" #aws provider
-            version = "~> 4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" #aws provider
+      version = "~> 4.0"
     }
+  }
 }
 
 #Define the provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
+}
+#Define vpc
+resource "aws_vpc" "nisha_vpc" {
+  cidr_block = "10.0.0/16"
+  tags = {
+    Name = "aws_nisha_vpc"
+  }
 }
