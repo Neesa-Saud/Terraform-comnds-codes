b3f7333fa9dcf4c96b07ea87eeb5191e33953e1d
Neesa-Saud <nishasaud2061@gmail.com>
2025-12-06 08:37:05 +0545

    variable

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+    description = "AMI for the EC2"
+    type =  string
+    default = "ami-1g456ht6784ft"
+}
+variable "regionname" {
+    description = "Region" 
+    type = string 
+    default = "us-east-1"
+}
